27f754bbdfc4382f9fb2f10e56c888bc38103161
KukuriBaagh <howtoccdemo@gmail.com>
2022-06-13 14:53:20 +0530

    Flask App Initial Commit

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim adipiscing',
+            'author':'Amit Arya',
+            'create_date':'22-06-2022'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body':'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim adipiscing',
+            'author':'Amit Kumar Arya',
+            'create_date':'22-06-2022'
+        },
+        {
+            'id': 3,
+            'title':'Article Three',
+            'body':'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim adipiscing',
+            'author':'Amit',
+            'create_date':'22-06-2022'
+        }
+    ]
+    
+    return articles
